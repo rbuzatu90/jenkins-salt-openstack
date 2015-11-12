5f02b9828e64cdc4d8be30c45850d943912efcae
Remus Buzatu <r.buzatu90@gmail.com>
2015-11-12 12:21:08 +0200

    Fixed some options

--- parse.py
+++ parse.py
@@ -7,6 +7,7 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser()
+# Envoronment options
 parser.add_argument('--env_name', required=True, help='env name', type=str)
 parser.add_argument('--os_version', required=True, help='openstack version', type=str)
 parser.add_argument('--db_engine', required=True, help='database engine', type=str)
@@ -14,6 +15,19 @@ parser.add_argument('--msg_queue_engine', required=True, help='messaging queue',
 parser.add_argument('--reset', required=True, help='reset type', type=str)
 parser.add_argument('--debug', required=True, help='debug enabled', type=str)
 parser.add_argument('--upgrade', required=True, help='upgrade enabled', type=str)
+parser.add_argument("--cinder_volumes_group_name", required=True, type=str)
+parser.add_argument("--cinder_volumes_path", required=True, type=str)
+parser.add_argument("--cinder_volumes_group_size", required=True, type=str)
+parser.add_argument("--cinder_loopback_device", required=True, type=str)
+parser.add_argument("--cpu_allocation_ratio", required=True, type=str)
+parser.add_argument("--ram_allocation_ratio", required=True, type=str)
+parser.add_argument("--glance_copy_from", required=True, type=str)
+parser.add_argument("--glance_disk_format", required=True, type=str)
+parser.add_argument("--glance_container_format", required=True, type=str)
+parser.add_argument("--glance_is_public", required=True, type=str)
+parser.add_argument("--glance_protected", required=True, type=str)
+
+# Credentials options
 parser.add_argument("--db_password", required=True, type=str)
 parser.add_argument("--message_queue_user", required=True, type=str)
 parser.add_argument("--message_queue_password", required=True, type=str)
@@ -39,19 +53,10 @@ parser.add_argument("--neutron_metadata_secret", required=True, type=str)
 parser.add_argument("--keystone_admin_token", required=True, type=str)
 parser.add_argument("--overwrite_all_password", required=True, type=str)
 parser.add_argument("--keystonerc_location", required=True, type=str)
-parser.add_argument("--cinder_volumes_group_name", required=True, type=str)
-parser.add_argument("--cinder_volumes_path", required=True, type=str)
-parser.add_argument("--cinder_volumes_group_size", required=True, type=str)
-parser.add_argument("--cinder_loopback_device", required=True, type=str)
-parser.add_argument("--cpu_allocation_ratio", required=True, type=str)
-parser.add_argument("--ram_allocation_ratio", required=True, type=str)
-parser.add_argument("--glance_copy_from", required=True, type=str)
-parser.add_argument("--glance_disk_format", required=True, type=str)
-parser.add_argument("--glance_container_format", required=True, type=str)
-parser.add_argument("--glance_is_public", required=True, type=str)
-parser.add_argument("--glance_protected", required=True, type=str)
+
+# Networking options
 parser.add_argument("--neutron_integration_bridge", required=True, type=str)
-parser.add_argument("--neutron_external_bridge", required=True, type=str)
+parser.add_argument("--neutron_external_bridge", required=True, type=str) # also sets type_drivers bridge
 parser.add_argument("--network_interface_name", required=True, type=str)
 parser.add_argument("--vlan_range", required=True, type=str)
 parser.add_argument("--tunelling_enabled", required=True, type=str)
@@ -80,17 +85,17 @@ config["message_queue_engine"] = args.msg_queue_engine
 config["system_upgrade"] = args.upgrade
 config["reset"] = args.reset
 config["debug"] = args.debug
-config["cinder_volumes_group_name"] = args.cinder_volumes_group_name
-config["cinder_volumes_path"] = args.cinder_volumes_path
-config["cinder_volumes_group_size"] = args.cinder_volumes_group_size
-config["cinder_loopback_device"] = args.cinder_loopback_device
-config["cpu_allocation_ratio"] = args.cpu_allocation_ratio
-config["ram_allocation_ratio"] = args.ram_allocation_ratio
-config["glance_copy_from"] = args.glance_copy_from
-config["glance_disk_format"] = args.glance_disk_format
-config["glance_container_format"] = args.glance_container_format
-config["glance_is_public"] = args.glance_is_public
-config["glance_protected"] = args.glance_protected
+config["cinder"]["volumes_group_name"] = args.cinder_volumes_group_name
+config["cinder"]["volumes_path"] = args.cinder_volumes_path
+config["cinder"]["volumes_group_size"] = args.cinder_volumes_group_size
+config["cinder"]["loopback_device"] = args.cinder_loopback_device
+config["nova"]["cpu_allocation_ratio"] = args.cpu_allocation_ratio
+config["nova"]["ram_allocation_ratio"] = args.ram_allocation_ratio
+config["glance"]["images"]["cirros"]["parameters"]["copy_from"] = args.glance_copy_from
+config["glance"]["images"]["cirros"]["parameters"]["disk_format"] = args.glance_disk_format
+config["glance"]["images"]["cirros"]["parameters"]["container_format"] = args.glance_container_format
+config["glance"]["images"]["cirros"]["parameters"]["is_public"] = args.glance_is_public
+config["glance"]["images"]["cirros"]["parameters"]["protected"] = args.glance_protected
 
 with open(salt_env_dir+"environment.sls", "w") as f:
     f.write(yaml.dump(config, default_flow_style=False))
@@ -98,31 +103,31 @@ with open(salt_env_dir+"environment.sls", "w") as f:
 
 with open(salt_env_dir+"credentials.sls", "r") as f:
     config = yaml.load(f)
-config["db_password"] = args.db_password
-config["message_queue_user"] = args.message_queue_user
-config["message_queue_password"] = args.message_queue_password
-config["nova_db_name"] = args.nova_db_name
-config["nova_db_username"] = args.nova_db_username
-config["nova_db_password"] = args.nova_db_password
-config["keystone_db_name"] = args.keystone_db_name
-config["keystone_db_user"] = args.keystone_db_user
-config["keystone_db_password"] = args.keystone_db_password
-config["cinder_db_name"] = args.cinder_db_name
-config["cinder_db_user"] = args.cinder_db_user
-config["cinder_db_password"] = args.cinder_db_password
-config["glance_db_name"] = args.glance_db_name
-config["glance_db_user"] = args.glance_db_user
-config["glance_db_password"] = args.glance_db_password
-config["neutron_db_name"] = args.neutron_db_name
-config["neutron_db_user"] = args.neutron_db_user
-config["neutron_db_password"] = args.neutron_db_password
-config["heat_db_name"] = args.heat_db_name
-config["heat_db_user"] = args.heat_db_user
-config["heat_db_password"] = args.heat_db_password
-config["neutron_metadata_secret"] = args.neutron_metadata_secret
-config["keystone_admin_token"] = args.keystone_admin_token
-config["overwrite_all_password"] = args.overwrite_all_password
-config["keystonerc_location"] = args.keystonerc_location
+config["mysql"]["root_password"] = args.db_password
+config["rabbitmq"]["user_name"] = args.message_queue_user
+config["rabbitmq"]["user_password"] = args.message_queue_password
+config["databases"]["nova"]["nova_db_name"] = args.nova_db_name
+config["databases"]["nova"]["nova_db_username"] = args.nova_db_username
+config["databases"]["nova"]["nova_db_password"] = args.nova_db_password
+config["databases"]["keystone"]["keystone_db_name"] = args.keystone_db_name
+config["databases"]["keystone"]["keystone_db_user"] = args.keystone_db_user
+config["databases"]["keystone"]["keystone_db_password"] = args.keystone_db_password
+config["databases"]["cinder"]["cinder_db_name"] = args.cinder_db_name
+config["databases"]["cinder"]["cinder_db_user"] = args.cinder_db_user
+config["databases"]["cinder"]["cinder_db_password"] = args.cinder_db_password
+config["databases"]["glance"]["glance_db_name"] = args.glance_db_name
+config["databases"]["glance"]["glance_db_user"] = args.glance_db_user
+config["databases"]["glance"]["glance_db_password"] = args.glance_db_password
+config["databases"]["neutron"]["neutron_db_name"] = args.neutron_db_name
+config["databases"]["neutron"]["neutron_db_user"] = args.neutron_db_user
+config["databases"]["neutron"]["neutron_db_password"] = args.neutron_db_password
+config["databases"]["heat"]["heat_db_name"] = args.heat_db_name
+config["databases"]["heat"]["heat_db_user"] = args.heat_db_user
+config["databases"]["heat"]["heat_db_password"] = args.heat_db_password
+config["neutron"]["metadata_secret"] = args.neutron_metadata_secret
+config["keystone"]["admin_token"] = args.keystone_admin_token
+#config["overwrite_all_password"] = args.overwrite_all_password
+config["keystone"]["tenants"]["admin"]["users"]["admin"]["keystonerc"]["path"] = args.keystonerc_location
 
 
 with open(salt_env_dir+"credentials.sls", "w") as f:
@@ -131,19 +136,19 @@ with open(salt_env_dir+"credentials.sls", "w") as f:
 
 with open(salt_env_dir+"networking.sls", "r") as f:
     config = yaml.load(f)
-config["neutron_integration_bridge"] = args.neutron_integration_bridge
-config["neutron_external_bridge"] = args.neutron_external_bridge
-config["network_interface_name"] = args.network_interface_name
-config["vlan_range"] = args.vlan_range
-config["tunelling_enabled"] = args.tunelling_enabled
-config["tunelling_type"] = args.tunelling_type
-config["tunneling_bridge"] = args.tunneling_bridge
-config["public_subnet_cidr"] = args.public_subnet_cidr
-config["public_subnet_allocation_pool_start"] = args.public_subnet_allocation_pool_start
-config["public_subnet_allocation_pool_end"] = args.public_subnet_allocation_pool_end
-config["public_subnet_dhcp_enabled"] = args.public_subnet_dhcp_enabled
-config["public_subnet_gateway_ip"] = args.public_subnet_gateway_ip
-config["private_subnet_cidr"] = args.private_subnet_cidr
+config["neutron"]["integration_bridge"] = args.neutron_integration_bridge
+config["neutron"]["external_bridge"] = args.neutron_external_bridge
+config["neutron"]["single_nic"]["interface"] = args.network_interface_name
+config["neutron"]["vlan"]["physnets"]["physnet1"]["vlan_range"] = args.vlan_range
+config["neutron"]["tunelling"]["enable"] = args.tunelling_enabled
+config["neutron"]["tunelling"]["types"] = args.tunelling_type
+config["neutron"]["tunneling"]["bridge"] = args.tunneling_bridge
+config["neutron"]["networks"]["public"]["subnets"]["public_subnet"]["cidr"] = args.public_subnet_cidr
+config["neutron"]["networks"]["public"]["subnets"]["public_subnet"]["allocation_pools"]["start"] = args.public_subnet_allocation_pool_start
+config["neutron"]["networks"]["public"]["subnets"]["public_subnet"]["allocation_pool"]["end"] = args.public_subnet_allocation_pool_end
+config["neutron"]["networks"]["public"]["subnets"]["public_subnet"]["subnet_dhcp_enabled"] = args.public_subnet_dhcp_enabled
+config["neutron"]["networks"]["public"]["subnets"]["public_subnet"]["subnet_gateway_ip"] = args.public_subnet_gateway_ip
+config["neutron"]["networks"]["private"]["subnets"]["private_subnet"]["cidr"] = args.private_subnet_cidr
 with open(salt_env_dir+"networking.sls", "w") as f:
     f.write(yaml.dump(config, default_flow_style=False))
 
